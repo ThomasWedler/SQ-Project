ca1c3981fdaeb4a554627063021eac479119c604
Kristof Korwisi <korwisi@googlemail.com>
2012-03-27 13:54:55 +0200

    Added isThumb and getThumb method to check if thumbnail exists and to get path and filename of thumbnail (last doesn't check if thumbnail exists, make sure it does :-)

--- src/optional/Thumbnails.scala
+++ src/optional/Thumbnails.scala
@@ -8,16 +8,49 @@ import java.awt.Color
 import java.awt.RenderingHints
 
 class Thumbnails {
-  // Generate thumbnail with mkThumb(file) where file is the filename and NOT path and filename..
+  // Check if thumbnail exists (file is just the filename and NOT path and filename...)
+  def isThumb(file: String): Boolean= {
+    var extension = file.substring(file.lastIndexOf(".") + 1)
+    var path = "filesystem/thumbnails/"
+    var thumbnailFile: File = null
+    if (extension == "jpg") {
+      path = path + "jpg/"
+      var thumbnailFile:  File = new File(path + file)
+    } else if (extension == "pdf") {
+      path = path + "pdf/"
+      var thumbnailFile:  File = new File(path + file)
+    } else if (extension == "mp4") {
+      path = path + "mp4/"
+      var thumbnailFile:  File = new File(path + file)
+    }
+    return thumbnailFile.exists
+  }
+  
+  // Get path and filename of thumbnail (file is just the filename and NOT path and filename...) (make sure thumbnail exists)
+  def getThumb(file: String): String= {
+    var extension = file.substring(file.lastIndexOf(".") + 1)
+    var path = "filesystem/thumbnails/"
+    var thumbnailFile = null
+    if (extension == "jpg") {
+      var thumbnailFile = path + file
+    } else if (extension == "pdf") {
+      var thumbnailFile = path + "pdf/" + file.split('.').init :+ "jpg" mkString "."
+    } else if (extension == "mp4") {
+      var thumbnailFile = path + "mp4/" + file.split('.').init :+ "jpg" mkString "."
+    }
+    return thumbnailFile
+  }
+  
+  // Generate thumbnail (file is just the filename and NOT path and filename...)
   def mkThumb(file: String) {
     var thumbWidth: Int = 100
     var thumbHeight: Int = 100
     var extension = file.substring(file.lastIndexOf(".") + 1)
     
     if (extension == "jpg") {
-      val path: String = "filesystem/jpg/"
+      val path = "filesystem/jpg/"
       var filename = path + file
-      var thumbnailFile: String = "filesystem/thumbnails/" + file
+      var thumbnailFile = "filesystem/thumbnails/jpg/" + file
       
       var image: Image = javax.imageio.ImageIO.read(new File(filename))
         
@@ -51,13 +84,13 @@ class Thumbnails {
         
       javax.imageio.ImageIO.write(thumbImage, "jpg", new File(thumbnailFile));
     } else if (extension == "pdf") {
-      val path: String = "filesystem/pdf/"
+      val path = "filesystem/pdf/"
       var filename = path + file
-      var thumbnailFile: String = "filesystem/thumbnails/" + file.split('.').init :+ "jpg" mkString "."      
+      var thumbnailFile = "filesystem/thumbnails/pdf/" + file.split('.').init :+ "jpg" mkString "."      
     } else if (extension == "mp4") {
-      val path: String = "filesystem/mp4/"
+      val path = "filesystem/mp4/"
       var filename = path + file
-      var thumbnailFile: String = "filesystem/thumbnails/" + file.split('.').init :+ "jpg" mkString "."      
+      var thumbnailFile = "filesystem/thumbnails/mp4/" + file.split('.').init :+ "jpg" mkString "."      
     }
   }
 }
