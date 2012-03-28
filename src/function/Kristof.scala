5822746a1802c81d7d823ca7583d7f6b01485229
Kristof Korwisi <korwisi@googlemail.com>
2012-03-28 13:19:51 +0200

    Code cleanup in Thumbnails.scala. Added comments to Thumbnails.scala. Removed Kristof.scala (no need).

--- src/function/Kristof.scala
+++
@@ -1,5 +0,0 @@
-package function
-
-class Kristof {
-
-}
--- src/optional/Thumbnails.scala
+++ src/optional/Thumbnails.scala
@@ -1,3 +1,4 @@
+/** This package provides optional features to SQ-Project */
 package optional
 
 import java.awt.Image
@@ -7,7 +8,11 @@ import java.awt.Graphics2D
 import java.awt.Color
 import java.awt.RenderingHints
 
-// "PDFRenderer only deals with up to version 1.4 of the PDF spec. The current version is 1.6, and there have been quite a few additions and changes between 1.4 and 1.6 which seem to break PDFRenderer."
+/*
+ * "PDFRenderer only deals with up to version 1.4 of the PDF spec.
+ * The current version is 1.6, and there have been quite a few additions
+ * and changes between 1.4 and 1.6 which seem to break PDFRenderer."
+ */
 import com.sun.pdfview.PDFFile
 import com.sun.pdfview.PDFPage
 
@@ -18,17 +23,18 @@ import java.awt.Rectangle
 
 // See http://www.capricasoftware.co.uk/vlcj/index.php for requirements and dependencies
 import java.util.concurrent.CountDownLatch
-
 import uk.co.caprica.vlcj.player.MediaPlayer
 import uk.co.caprica.vlcj.player.MediaPlayerEventAdapter
 import uk.co.caprica.vlcj.player.MediaPlayerFactory
-//import uk.co.caprica.vlcj.test.VlcjTest
 
-
-
+/** Provides methods for creating and managing thumbnails */
 class Thumbnails {
-  // Check if thumbnail exists (file is just the filename and NOT path and filename...)
-  def isThumb(file: String): Boolean= {
+  /** Checks if thumbnail of given file exists.
+    *
+    * @param file filename (don't include path) (String).
+    * @return if thumbnail for given file already exists (Boolean).
+    */
+  def isThumb(file: String): Boolean = {
     var extension = file.substring(file.lastIndexOf(".") + 1)
     var path = "filesystem/thumbnails/"
     var thumbnailFile: File = new File("")
@@ -42,13 +48,22 @@ class Thumbnails {
       path = path + "mp4/"
       thumbnailFile = new File(path + file)
     } else if (extension == "txt") {
+      /* 
+       * txt-files are for relations only and therefor will not have a thumbnail.
+       * To avoid thumbnail-generation true is returned even it's not :-)
+       */
       return true
     }
     return thumbnailFile.exists
   }
-  
-  // Get path and filename of thumbnail (file is just the filename and NOT path and filename...) (make sure thumbnail exists)
-  def getThumb(file: String): String= {
+
+  /** Gets path and filename of thumbnail.
+    * Make sure thumbnail exists.
+    *
+    * @param file filename (don't include path) (String).
+    * @return path and filename of thumbnail (String).
+    */
+  def getThumb(file: String): String = {
     var extension = file.substring(file.lastIndexOf(".") + 1)
     var path = "filesystem/thumbnails/"
     var thumbnailFile = ""
@@ -62,86 +77,94 @@ class Thumbnails {
     }
     return thumbnailFile
   }
-  
-  // Generate thumbnail (file is just the filename and NOT path and filename...)
+
+  /** Generates thumbnail and writes it to disc.
+    *
+    * @param file filename (don't include path) (String).
+    */
   def mkThumb(file: String) {
+    // Width and height of thumbnail
     var thumbWidth: Int = 100
     var thumbHeight: Int = 100
+
     var extension = file.substring(file.lastIndexOf(".") + 1)
     var image: Image = null
     var thumbnailFile: String = ""
-    
+
     // Generate thumbnail of a JPEG-file
     if (extension == "jpg") {
       val path = "filesystem/jpg/"
       var filename = path + file
       thumbnailFile = "filesystem/thumbnails/jpg/" + file
-      
+
       image = javax.imageio.ImageIO.read(new File(filename))
 
-    // Generate thumbnail of a PDF-file
+      // Generate thumbnail of a PDF-file
     } else if (extension == "pdf") {
       val path = "filesystem/pdf/"
       var filename = path + file
       thumbnailFile = "filesystem/thumbnails/pdf/" + file.split('.').init :+ "jpg" mkString "."
-      
+
       var pdffilename: File = new File(filename)
       var raf: RandomAccessFile = new RandomAccessFile(pdffilename, "r")
       var channel: FileChannel = raf.getChannel()
       var buf: ByteBuffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size())
       var pdffile: PDFFile = new PDFFile(buf)
-      
-      // draw first page to an image
+
+      // Draw first page to an image
       var page: PDFPage = pdffile.getPage(0)
-      
-      //get width and height for the doc at the default zoom
-      var rect: Rectangle = new Rectangle(0,0,page.getBBox().getWidth().toInt, page.getBBox().getHeight().toInt)
-      
-      //generate image
+
+      // Get width and height for the doc at the default zoom
+      var rect: Rectangle = new Rectangle(0, 0, page.getBBox().getWidth().toInt, page.getBBox().getHeight().toInt)
+
+      // Generate image
       image = page.getImage(rect.width, rect.height, rect, null, true, true)
-      
-    // Generate thumbnail of an MPEG4-file
+
+      // Generate thumbnail of an MPEG4-file
     } else if (extension == "mp4") {
       val path = "filesystem/mp4/"
       var filename = path + file
       var thumbnailFile = "filesystem/thumbnails/mp4/" + file.split('.').init :+ "jpg" mkString "."
-      
-      // via http://code.google.com/p/vlcj/source/browse/trunk/vlcj/src/test/java/uk/co/caprica/vlcj/test/thumbs/ThumbsTest.java
-      
+
+      /*
+       * The following is an adaption of
+       * http://code.google.com/p/vlcj/source/browse/trunk/vlcj/src/test/java/uk/co/caprica/vlcj/test/thumbs/ThumbsTest.java
+       */
       val VLC_THUMBNAIL_POSITION: Float = 30.0f / 100.0f
-      
+
       // Set media resource locator
       val mrl: String = "file://" + filename
-      
-      var factory: MediaPlayerFactory = new MediaPlayerFactory("--intf", "dummy",          /* no interface */
-                                                               "--vout", "dummy",          /* we don't want video (output) */
-                                                               "--no-audio",               /* we don't want audio (decoding) */
-                                                               "--no-video-title-show",    /* nor the filename displayed */
-                                                               "--no-stats",               /* no stats */
-                                                               "--no-sub-autodetect-file", /* we don't want subtitles */
-                                                               "--no-inhibit",             /* we don't want interfaces */
-                                                               "--no-disable-screensaver", /* we don't want interfaces */
-                                                               "--no-snapshot-preview"     /* no blending in dummy vout */)
-      
+
+      var factory: MediaPlayerFactory = new MediaPlayerFactory(
+        "--intf", "dummy", /* no interface */
+        "--vout", "dummy", /* we don't want video (output) */
+        "--no-audio", /* we don't want audio (decoding) */
+        "--no-video-title-show", /* nor the filename displayed */
+        "--no-stats", /* no stats */
+        "--no-sub-autodetect-file", /* we don't want subtitles */
+        "--no-inhibit", /* we don't want interfaces */
+        "--no-disable-screensaver", /* we don't want interfaces */
+        "--no-snapshot-preview" /* no blending in dummy vout */ )
+
       var mediaPlayer: MediaPlayer = factory.newHeadlessMediaPlayer();
-      
+
       val inPositionLatch: CountDownLatch = new CountDownLatch(1)
       val snapshotTakenLatch: CountDownLatch = new CountDownLatch(1)
-      
+
       mediaPlayer.addMediaPlayerEventListener(new MediaPlayerEventAdapter() {
         override def positionChanged(mediaPlayer: MediaPlayer, newPosition: Float) {
-          if(newPosition >= VLC_THUMBNAIL_POSITION * 0.9f) { /* 90% margin */
+          if (newPosition >= VLC_THUMBNAIL_POSITION * 0.9f) { /* 90% margin */
             inPositionLatch.countDown()
           }
         }
-        
+
         override def snapshotTaken(mediaPlayer: MediaPlayer, filename: String) {
           // Debugging
           System.out.println("snapshotTaken(filename=" + filename + ")")
           snapshotTakenLatch.countDown()
         }
       })
-      
+
       if (mediaPlayer.startMedia(mrl)) {
         mediaPlayer.setPosition(VLC_THUMBNAIL_POSITION)
         inPositionLatch.await() // Might wait forever if error
@@ -149,31 +172,31 @@ class Thumbnails {
         snapshotTakenLatch.await() // Might wait forever if error
         mediaPlayer.stop()
       }
-      
+
       mediaPlayer.release()
       factory.release()
     }
-    
-    var thumbRatio: Double = thumbWidth.toDouble/thumbHeight.toDouble
+
+    var thumbRatio: Double = thumbWidth.toDouble / thumbHeight.toDouble
     var imageWidth: Int = image.getWidth(null)
     var imageHeight: Int = image.getHeight(null)
-    var imageRatio: Double = imageWidth.toDouble/imageHeight.toDouble
-      
-    if (thumbRatio < imageRatio){
-      thumbHeight = (thumbWidth/imageRatio).toInt      
+    var imageRatio: Double = imageWidth.toDouble / imageHeight.toDouble
+
+    if (thumbRatio < imageRatio) {
+      thumbHeight = (thumbWidth / imageRatio).toInt
     } else {
-      thumbWidth = (thumbHeight*imageRatio).toInt      
+      thumbWidth = (thumbHeight * imageRatio).toInt
     }
-    
-    if (imageWidth < thumbWidth && imageHeight < thumbHeight){
+
+    if (imageWidth < thumbWidth && imageHeight < thumbHeight) {
       thumbWidth = imageWidth
-      thumbHeight = imageHeight;     
+      thumbHeight = imageHeight;
     } else if (imageWidth < thumbWidth) {
       thumbWidth = imageWidth
     } else if (imageHeight < thumbHeight) {
       thumbHeight = imageHeight
     }
-    
+
     var thumbImage: BufferedImage = new BufferedImage(thumbWidth, thumbHeight, BufferedImage.TYPE_INT_RGB)
     var graphics2D: Graphics2D = thumbImage.createGraphics()
     graphics2D.setBackground(Color.WHITE)
@@ -183,6 +206,6 @@ class Thumbnails {
     graphics2D.drawImage(image, 0, 0, thumbWidth, thumbHeight, null)
 
     javax.imageio.ImageIO.write(thumbImage, "jpg", new File(thumbnailFile));
-    
+
   }
 }
