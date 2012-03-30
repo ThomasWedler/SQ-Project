e3ae9fb4edbebf229fdca4e42dd92c7a9d34a397
Kristof Korwisi <korwisi@googlemail.com>
2012-03-30 17:25:55 +0200

    Fixed output of setReader() and added some exception handling (tryConfig()).

--- src/basic/Config.scala
+++ src/basic/Config.scala
@@ -2,17 +2,67 @@ package basic
 
 import scala.xml._
 import org.apache.commons.io.FileUtils
-import java.io.File
+import java.io.{ File, IOException }
 
 /** Provides methods for reading and editing the configuration file */
 class Config {
+  // Set path and filename of configuration file
+  val configFile = "config.xml"
+
+  /** Checks if configuration file exits and contains valid values.
+    * In case file doesn't exists it is created and filled with standard values.
+    */
+  def tryConfig() {
+    if (!(new File(configFile).exists())) {
+      val file: File = new File(configFile)
+      try {
+        new File(configFile).createNewFile()
+      } catch {
+        case io: IOException => println("No configuration file found. Failed to create a new one.")
+      }
+      var config = scala.xml.XML.loadFile(configFile)
+      val newjpgread = Elem(null, "read", Null, TopScope, Text(""))
+      val newjpgedit = Elem(null, "edit", Null, TopScope, Text(""))
+      val newpdfread = Elem(null, "read", Null, TopScope, Text(""))
+      val newpdfedit = Elem(null, "edit", Null, TopScope, Text(""))
+      val newmp4read = Elem(null, "read", Null, TopScope, Text(""))
+      val newmp4edit = Elem(null, "edit", Null, TopScope, Text(""))
+      val newjpg = Elem(null, "jpg", Null, TopScope, newjpgread, newjpgedit)
+      val newpdf = Elem(null, "jpg", Null, TopScope, newpdfread, newpdfedit)
+      val newmp4 = Elem(null, "jpg", Null, TopScope, newmp4read, newmp4edit)
+      val newconfig = Elem(null, "config", Null, TopScope, newjpg, newpdf, newmp4)
+      var prettyfier = new scala.xml.PrettyPrinter(80, 2)
+      val header = """<?xml version="1.0" encoding="UTF-8" ?>""" + "\n"
+      val prettyConfig = header + prettyfier.format(config)
+      val xmlFile: File = new File(configFile)
+      try {
+        FileUtils.write(xmlFile, prettyConfig, "UTF-8")
+      } catch {
+        case io: IOException => println("No configuration file found. Created a new one but failed to fill it.")
+      }
+    } else {
+      try {
+        var config = scala.xml.XML.loadFile(configFile)
+        val jpgread = (config \\ "config" \\ "jpg" \ "read").text
+        val jpgedit = (config \\ "config" \\ "jpg" \ "edit").text
+        val pdfread = (config \\ "config" \\ "pdf" \ "read").text
+        val pdfedit = (config \\ "config" \\ "pdf" \ "edit").text
+        val mp4read = (config \\ "config" \\ "mp4" \ "read").text
+        val mp4edit = (config \\ "config" \\ "mp4" \ "edit").text
+      } catch {
+        case wrongXML: Exception => println("Configuration file is broken. Remove it and retry again.")
+      }
+    }
+  }
+
   /** Returns reader for given filetype.
     *
     * @param filetype (String).
     * @return reader for given filetype (String).
     */
   def getReader(filetype: String): String = {
-    val config = scala.xml.XML.loadFile("config.xml")
+    tryConfig()
+    val config = scala.xml.XML.loadFile(configFile)
     if (filetype == "jpg") {
       return (config \\ "config" \\ "jpg" \ "read").text
     } else if (filetype == "pdf") {
@@ -30,7 +80,8 @@ class Config {
     * @return editor for given filetype (String).
     */
   def getEditor(filetype: String): String = {
-    val config = scala.xml.XML.loadFile("config.xml")
+    tryConfig()
+    val config = scala.xml.XML.loadFile(configFile)
     if (filetype == "jpg") {
       return (config \\ "config" \\ "jpg" \ "edit").text
     } else if (filetype == "pdf") {
@@ -48,7 +99,8 @@ class Config {
     * @param reader (String).
     */
   def setReader(filetype: String, reader: String) {
-    var config = scala.xml.XML.loadFile("config.xml")
+    tryConfig()
+    var config = scala.xml.XML.loadFile(configFile)
     val jpgread = (config \\ "config" \\ "jpg" \ "read").text
     val jpgedit = (config \\ "config" \\ "jpg" \ "edit").text
     val pdfread = (config \\ "config" \\ "pdf" \ "read").text
@@ -84,7 +136,11 @@ class Config {
 
     val newconfig = Elem(null, "config", Null, TopScope, newjpg, newpdf, newmp4)
 
-    scala.xml.XML.save("config.xml", newconfig)
+    var prettyfier = new scala.xml.PrettyPrinter(80, 2)
+    val header = """<?xml version="1.0" encoding="UTF-8" ?>""" + "\n"
+    val prettyConfig = header + prettyfier.format(newconfig)
+    val xmlFile: File = new File(configFile)
+    FileUtils.write(xmlFile, prettyConfig, "UTF-8")
   }
 
   /** Sets given editor for given filetype.
@@ -93,7 +149,8 @@ class Config {
     * @param editor (String).
     */
   def setEditor(filetype: String, editor: String) {
-    var config = scala.xml.XML.loadFile("config.xml")
+    tryConfig()
+    var config = scala.xml.XML.loadFile(configFile)
     val jpgread = (config \\ "config" \\ "jpg" \ "read").text
     val jpgedit = (config \\ "config" \\ "jpg" \ "edit").text
     val pdfread = (config \\ "config" \\ "pdf" \ "read").text
@@ -131,9 +188,9 @@ class Config {
 
     var prettyfier = new scala.xml.PrettyPrinter(80, 2)
     val header = """<?xml version="1.0" encoding="UTF-8" ?>""" + "\n"
-    val prettyConfig = header + prettyfier.format(config)
-    val xmlFile: File = new File("config.xml")
-    FileUtils.write(xmlFile, prettyConfig, "UTF-8")    
+    val prettyConfig = header + prettyfier.format(newconfig)
+    val xmlFile: File = new File(configFile)
+    FileUtils.write(xmlFile, prettyConfig, "UTF-8")
   }
 
 }
